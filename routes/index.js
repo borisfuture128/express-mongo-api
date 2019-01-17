166678a342135b9dfc9e23ab4d31efe81a2e6a9c
Travis DeveloperTra <you@example.com>
2019-01-17 02:25:21 -0800

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,45 @@ var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("index.js /")
-  //var collection = db.get().collection('customers')
+  var RecordType = req.query.RecordType
+  var DataName = req.query.DataName
+  console.log(RecordType)
+  console.log(DataName)
   var collection = db.get().collection('customers')
-  collection.find().toArray(function(err, docs) {
-    //res.render('comments', {comments: docs})
-    console.log(docs)
-  })
+  if(RecordType != undefined && DataName != undefined){
+    // SELECT * FROM collection WHERE RecordType LIKE "%{pattern}%" AND Data.Name LIKE "%{pattern}%"  
+    console.log('SELECT * FROM collection WHERE RecordType LIKE "%{pattern}%" AND Data.Name LIKE "%{pattern}%"')
+    var pattern1 = RecordType
+    var pattern2 = DataName
+    collection.find({"RecordType": {$regex:pattern1 }, "Data.Name": {$regex:pattern2}}, {_id:0}).toArray(function(err, docs) {
+      console.log(docs)
+      res.json({"result":docs})
+    })
+    return;
+  }
 
-  res.json({"result":"Ok"})
+  if(RecordType != undefined && DataName == undefined){
+    // SELECT * FROM collection WHERE RecordType LIKE "%{pattern}%"
+    console.log('SELECT * FROM collection WHERE RecordType LIKE "%{pattern}%"')
+    var pattern = RecordType
+    collection.find( { "RecordType": {$regex:pattern } }).project({_id:0}).toArray(function(err, docs){
+      console.log(docs)
+      res.json({"result":docs})
+    })
+    return;
+  }
+
+  if(RecordType == undefined && DataName != undefined){
+    // SELECT * FROM collection WHERE Data.Name LIKE "%{pattern}%"  
+    console.log('SELECT * FROM collection WHERE Data.Name LIKE "%{pattern}%"')
+    var pattern = DataName
+    collection.find({ "Data.Name": {$regex:pattern }}, {_id:0}).toArray(function(err, docs) {
+      console.log(docs)
+      res.json({"result":docs})
+    })
+    return;
+  }
+  res.json({"result":"No result"})
   //res.render('index', { title: 'Express' });
 });
 
